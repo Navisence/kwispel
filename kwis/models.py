99137b40d5b57d75eeccaae2079bfdcab27488a1
Bart Brosens <bart.brosens@sigasi.com>
2025-07-07 20:21:26 +0200

    update for models

--- kwis/models.py
+++ kwis/models.py
@@ -1,12 +1,12 @@
 from django.db import models
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 
 # These models are defined:
 # - Teams participating
 # - Rounds in the quiz
 # - Answered score per round for each team
 
-class QTeam(models.Model):
+class Team(models.Model):
     # A team only needs a name for identification
     team_name = models.CharField(max_length=200, unique=True)
     def __str__(self):
@@ -15,7 +15,7 @@ class QTeam(models.Model):
         verbose_name = _('Team')
         verbose_name_plural = _('Teams')
 
-class QRound(models.Model):
+class Round(models.Model):
     # Each round has a name and a maximal score
     round_name = models.CharField(max_length=200, unique=True)
     max_score = models.DecimalField(max_digits=6, decimal_places=1)
@@ -25,14 +25,13 @@ class QRound(models.Model):
         verbose_name = _('Round')
         verbose_name_plural = _('Rounds')
 
-class QAnswer(models.Model):
+class Answer(models.Model):
     # Each answer is bound to a team, a round and contains the score for a specific team and round
-    team = models.ForeignKey(QTeam)
-    rnd = models.ForeignKey(QRound)
+    team = models.ForeignKey(Team, on_delete=models.CASCADE)
+    rnd = models.ForeignKey(Round, on_delete=models.CASCADE)
     score = models.DecimalField(max_digits=6, decimal_places=1)
     def __str__(self):
         return str(self.team) + ", " + self.rnd.round_name + ": " + str(self.score)
     class Meta:
         verbose_name = _('Answer')
         verbose_name_plural = _('Answers')
-
