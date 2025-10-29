e68cb4e995676b127bc2aa83da5ba7d66f13049f
Bart Brosens <bart.brosens@gmail.com>
2025-10-29 17:04:57 +0100

    persist database in docker volume

--- app/entrypoint.prod.sh
+++ app/entrypoint.prod.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
 # Check whether the database exists, and create it if not
-if [ ! -f /data/db.sqlite3 ]; then
+if [ ! -f /home/app/web/data/db.sqlite3 ]; then
     echo "Creating initial database"
     python manage.py migrate
     # Create an initial superuser
-    python manage.py createsuperuser --noinput --username admin
+#    python manage.py createsuperuser --noinput
     # Collect static files
     python manage.py collectstatic --noinput
 fi
--- app/kwispel/settings.py
+++ app/kwispel/settings.py
@@ -85,7 +85,7 @@ WSGI_APPLICATION = 'kwispel.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / 'data/db.sqlite3',
     }
 }
 
